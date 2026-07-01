4da0a718f26aa0925a3ff3086a69e1e9e5e1820f
Colleen Murray <colleenkhm@gmail.com>
2026-07-01 07:18:22 -0500

    add automatic scroll to top on navigation to new screen

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,10 +1,12 @@
 import Header from "./routes/Header";
 import Footer from "./routes/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
